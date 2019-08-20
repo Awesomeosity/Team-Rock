1277c45ebf34d91e0be52ebb2ca629d5efe33979
Kevin Le <infernapeman@gmail.com>
2019-08-20 15:59:51 -0600

    Basic getters/setters

--- Src/GameObjects/GameObject.cs
+++ Src/GameObjects/GameObject.cs
@@ -15,5 +15,73 @@ namespace TeamRock.Src.GameObjects
         private Vector2 _position;
         private Vector2 _velocity;
         private Vector2 _acceleration;
+
+        public GameObject(Texture2D sprite, Vector2 position, Vector2 velocity, Vector2 acceleration)
+        {
+            _sprite = sprite;
+            _position = position;
+            _velocity = velocity;
+            _acceleration = acceleration;
+        }
+
+        public Texture2D Sprite
+        {
+            get
+            {
+                return _sprite;
+            }
+            
+            set
+            {
+                _sprite = value;
+            }
+        }
+
+        public Vector2 Position
+        {
+            get
+            {
+                return _position;
+            }
+
+            set
+            {
+                _position = value;
+            }
+        }
+
+        public Vector2 Velocity
+        {
+            get
+            {
+                return _velocity;
+            }
+
+            set
+            {
+                _velocity = value;
+            }
+        }
+
+        public Vector2 Acceleration
+        {
+            get
+            {
+                return _acceleration;
+            }
+
+            set
+            {
+                _acceleration = value;
+            }
+        }
+
+        public Vector2 getNewPosition(double totalSeconds)
+        {
+            Vector2 deltaVelocity = _acceleration * (float)totalSeconds;
+            _velocity += deltaVelocity;
+            _position += _velocity;
+            return _position;
+        }
     }
 }
