4fe6059fc4ea44ca26119d5d0d8c5700a6e46e8f
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2019-08-20 13:15:05 -0600

    Adding GameObject

---
+++ Src/GameObjects/GameObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TeamRock.Src.GameObjects
+{
+    class GameObject
+    {
+        private Texture2D _sprite;
+
+        private Vector2 _position;
+        private Vector2 _velocity;
+        private Vector2 _acceleration;
+    }
+}
